80f31b3ecaaea87b66b80275b11d8c2e0d0e3132
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-05 16:16:18 +0530

    Fix manifest file

--- feedback_form/__manifest__.py
+++ feedback_form/__manifest__.py
@@ -3,17 +3,24 @@
     "version": "18.0.1.0.0",
     "summary": "Simple feedback form with rating",
     "category": "Tools",
+    "author": "Odoo",
+    "license": "LGPL-3",
     "depends": ["base"],
+
     "data": [
-         "security/ir.model.access.csv",
-         "data/sequence.xml",
-         "views/feedback_view.xml",
+        "security/ir.model.access.csv",
+        "data/sequence.xml",
+        "views/feedback_view.xml",
     ],
+
     "assets": {
         "web.assets_backend": [
             "feedback_form/static/src/css/feedback.css",
         ],
     },
+
+    "images": ["static/description/cover.png"],
+
     "installable": True,
     "application": False,
 }
