3be57165d148be7b44ff6b399b697268d748929d
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-05 16:54:16 +0530

    Manifest File

--- feedback_form/__manifest__.py
+++ feedback_form/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "18.0.1.0.0",
     "summary": "Simple feedback form with rating",
     "category": "Tools",
-    "author": "Odoo",
+    "author": "AppForge",
     "license": "LGPL-3",
     "depends": ["base"],
 
