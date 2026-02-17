8d43433ab81d087c087388cc52f73a491d68c229
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-17 16:17:04 +0530

    Update

--- feedback_form/__manifest__.py
+++ feedback_form/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": "Simple feedback form with rating",
     "category": "Tools",
     "author": "AppForge",
-    "license": "LGPL-3",
+    "license": "OPL-1",
     "website": "https://www.odoo.com",
 
     "depends": [
@@ -32,6 +32,7 @@
     "installable": True,
     "application": False,
 }
+
 
 
 
