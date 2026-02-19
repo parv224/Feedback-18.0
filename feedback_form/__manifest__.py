6d6a7ba12db1869f1d5027e7e5c1cbfb0b880049
Parv Prajapati <158603903+parv224@users.noreply.github.com>
2026-02-19 14:45:15 +0530

    Update __manifest__.py
    Updated the price of the feedback form from 16.96 to 12.50.

--- feedback_form/__manifest__.py
+++ feedback_form/__manifest__.py
@@ -12,7 +12,7 @@
         "mail"
     ],
 
-    "price": 16.96,
+    "price": 12.50,
     "currency": "EUR",
 
     "data": [
@@ -32,6 +32,7 @@
     "installable": True,
     "application": False,
 }
+
 
 
 
